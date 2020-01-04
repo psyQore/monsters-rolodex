6264cf664a27607976becfb557aeccda6ee73aa6
juan galarce <j6alarce@gmail.com>
2020-01-03 21:41:53 -0300

    Updates

---
+++ precache-manifest.9d6b75909678020140f14f34203cd988.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ecc293cb40a9e0d008b3bbe1dad629",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3546b1a26d26030bdca6",
+    "url": "/monsters-rolodex/static/css/main.69677df7.chunk.css"
+  },
+  {
+    "revision": "5f64445c3b396716b777",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3546b1a26d26030bdca6",
+    "url": "/monsters-rolodex/static/js/main.6177e9a7.chunk.js"
+  },
+  {
+    "revision": "8e0631e8fb00ca5a6969",
+    "url": "/monsters-rolodex/static/js/runtime-main.41315a6c.js"
+  }
+]);
